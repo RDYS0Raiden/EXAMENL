55c2f68c9294b8c4cb5ca05144a9828e82f3f945
RDYS0Raiden <luis0ticona2@gmail.com>
2022-09-23 23:13:57 -0300

    Initial commit

---
+++ app/src/main/java/com/example/examenl/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.examenl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.core.view.isVisible
+import com.example.examenl.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var myHandler:Handler
+
+    var fibonacci= mutableListOf<Int>(10)
+
+    var numero:String=""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        myHandler=Handler(mainLooper)
+        setContentView(binding.root)
+        binding.btnprocesar.setOnClickListener {
+            Hilo()
+            Fibonacci()
+            resultado()
+        }
+
+    }
+    private fun Hilo(){
+        var numero2:String=""
+
+        Thread{
+            try {
+                for (i in 1..10){
+                    Thread.sleep(500)
+                    myHandler.post{
+                        binding.apply {
+                            txtconteo.text = "$i"
+                            pltxtnumero.isVisible =true
+                            numero2 = binding.pltxtnumero.text.toString()
+                        }
+                    }
+                }
+                myHandler.post{
+                    binding.pltxtnumero.isVisible=true
+                    if (numero == numero2){
+                        binding.btnpasar.isVisible=true
+                        binding.txtconteo.text = "Usted Gano"
+                    }else{
+                        binding.txtconteo.text = "Usted perdio siga intentando"
+                    }
+                }
+
+            } catch (e: InterruptedException){
+                e.printStackTrace()
+            }
+        }.start()
+    }
+    private fun Fibonacci(){
+        var fib = 0
+        var aux = 1
+        for(i in 0 .. 10){
+            aux += fib
+            fib = aux-fib
+            fibonacci.add(i,fib)
+        }
+    }
+    private fun resultado(){
+        var aleatorio:Int = (0..9).random()
+        numero = fibonacci[aleatorio].toString()
+        binding.txtmostrar.text = numero
+    }
+}
